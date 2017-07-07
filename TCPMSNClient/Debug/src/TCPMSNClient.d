6e7e2ed50aa82aeff7130a22e774c7481e377987
Idan <idanl22@gmail.com>
2017-07-07 12:12:21 +0300

    TCPMSNClient

---
+++ TCPMSNClient/Debug/src/TCPMSNClient.d
@@ -0,0 +1,21 @@
+src/TCPMSNClient.d: ../src/TCPMSNClient.cpp ../src/TCPMSNClient.h \
+ ../src/TCPMessengerProtocol.h \
+ /home/user/Desktop/workspaceLab3/SocketUtils/src/TCPSocket.h \
+ /home/user/Desktop/workspaceLab3/SocketUtils/src/Socket.h \
+ /home/user/Desktop/workspaceLab3/SocketUtils/src/MThread.h \
+ /home/user/Desktop/workspaceLab3/SocketUtils/src/FileInterface.h \
+ /home/user/Desktop/workspaceLab3/SocketUtils/src/MThread.h
+
+../src/TCPMSNClient.h:
+
+../src/TCPMessengerProtocol.h:
+
+/home/user/Desktop/workspaceLab3/SocketUtils/src/TCPSocket.h:
+
+/home/user/Desktop/workspaceLab3/SocketUtils/src/Socket.h:
+
+/home/user/Desktop/workspaceLab3/SocketUtils/src/MThread.h:
+
+/home/user/Desktop/workspaceLab3/SocketUtils/src/FileInterface.h:
+
+/home/user/Desktop/workspaceLab3/SocketUtils/src/MThread.h:
